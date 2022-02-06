a5181ba0d5f8e54929cb029a7bb356b9edd302d0
Jacek Karczmarczyk <jk@drukcd.pl>
2022-02-06 13:30:42 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,5 @@
 <template>
   <div>
-    Build time: {{ buildTime }}
     <router-view />
   </div>
 </template>
-
-<script lang="ts">
-import { BUILD_TIME } from './env';
-import { defineComponent } from '@vue/composition-api';
-
-export default defineComponent({
-  name: 'App',
-  setup () {
-    return {
-      buildTime: BUILD_TIME,
-    };
-  },
-});
-</script>
