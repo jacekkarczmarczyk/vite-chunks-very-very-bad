0ce108dc50f9120c382911722622268396bbebcd
Jacek Karczmarczyk <jk@drukcd.pl>
2022-11-07 14:42:46 +0100

    build

---
+++ dist/assets/Home.064c5be1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index.2f93922c.js";const r={};function n(t,a){return c(),o("div",null,"Home")}var s=e(r,[["render",n]]);export{s as default};
---
+++ dist/assets/View1.2fe9e74d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index.2f93922c.js";const o={};function n(t,a){return c(),r("div",null,"Home")}var s=e(o,[["render",n]]);export{s as default};
